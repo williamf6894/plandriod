a09c386084f8ba25bcea5e2a9cf06bb33182df40
Will Flaherty <williamf6894@gmail.com>
2016-04-29 20:56:11 +0100

    Added Repeat

--- app/src/main/java/com/williamf6894/plandroid/ReminderTime.java
+++ app/src/main/java/com/williamf6894/plandroid/ReminderTime.java
@@ -6,45 +6,43 @@ package com.williamf6894.plandroid;
 public class ReminderTime {
 
     private final int day = 1000 * 3600 * 24;
-    private int time1;
-    private int time2;
-    private int time3;
-    private int time4;
-    private int time5;
-    private int time6;
+
 
 
     ReminderTime(){
         //This will be the default time for Study
-
-
-        this.time1 = 2 * day;  /* One Day */
-        this.time2 = 2 * day;  /* One Week */
-        this.time3 = 2 * day;  /* One Month */
-        this.time4 = 2 * day;  /* Three Months */
-        this.time5 = 2 * day;  /* Six Months */
     }
 
     ReminderTime(int time1){
-        this.time1 = time1 * day;
+        time1 = time1 * day;
 
     }
 
+    // CreateStudy(){
+    //     long time1 = day;  /* One Day */
+    //     long time2 = 7 * day;  /* One Week */
+    //     long time3 = 30 * day;  /* One Month */
+    //     long time4 = 90 * day;  /* Three Months */
+    //     long time5 = 180 * day;  /* Six Months */
+    // }
+    //
+    //
+    //
+    //
+    //
+    // public void to_reminder(View view)
+    // {
+    //     Intent intent=new Intent(this,Notification_morning.class);
+    //     AlarmManager manager=(AlarmManager)getSystemService(Activity.ALARM_SERVICE);
+    //     PendingIntent pendingIntent=PendingIntent.getService(this,
+    //             0,intent, 0);
+    //     Calendar cal=Calendar.getInstance();
+    //     cal.set(Calendar.HOUR_OF_DAY, timepicker.getCurrentHour());
+    //     cal.set(Calendar.MINUTE,timepicker.getCurrentMinute());
+    //     cal.set(Calendar.SECOND, 0);
+    //     cal.set(Calendar.MILLISECOND, 0);
+    //     manager.setRepeating(AlarmManager.RTC_WAKEUP,cal.getTimeInMillis(),24*60*60*1000,pendingIntent);
+    //
+    // }
 
-    /*
-    public void to_reminder(View view)
-    {
-        Intent intent=new Intent(this,Notification_morning.class);
-        AlarmManager manager=(AlarmManager)getSystemService(Activity.ALARM_SERVICE);
-        PendingIntent pendingIntent=PendingIntent.getService(this,
-                0,intent, 0);
-        Calendar cal=Calendar.getInstance();
-        cal.set(Calendar.HOUR_OF_DAY, timepicker.getCurrentHour());
-        cal.set(Calendar.MINUTE,timepicker.getCurrentMinute());
-        cal.set(Calendar.SECOND, 0);
-        cal.set(Calendar.MILLISECOND, 0);
-        manager.setRepeating(AlarmManager.RTC_WAKEUP,cal.getTimeInMillis(),24*60*60*1000,pendingIntent);
-
-    }
-    */
 }
